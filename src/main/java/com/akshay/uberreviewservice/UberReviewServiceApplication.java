8d67a8a726d48cd603e9f788d5d93a97e848e0bc
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2024-10-21 22:53:50 +0530

    added a new table review with a column id as primary key

--- src/main/java/com/akshay/uberreviewservice/UberReviewServiceApplication.java
+++ src/main/java/com/akshay/uberreviewservice/UberReviewServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UberReviewServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UberReviewServiceApplication.class, args);
 	}
