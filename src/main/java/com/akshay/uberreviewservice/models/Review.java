c7b33755bd97c7d6332425e1e4137be120ea1186
Akshay-Jhijhautia <akshay.jhijhautia@gmail.com>
2024-10-22 00:21:29 +0530

    added a few more columns

--- src/main/java/com/akshay/uberreviewservice/models/Review.java
+++ src/main/java/com/akshay/uberreviewservice/models/Review.java
@@ -1,10 +1,31 @@
 package com.akshay.uberreviewservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "bookingreview")
 public class Review {
+
     @Id // this annotation makes the id property a primary key of our table
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // identity means auto increment
     Long id;
+
+    @Column(nullable = false)
+    String content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    Date updateAt;
 }
